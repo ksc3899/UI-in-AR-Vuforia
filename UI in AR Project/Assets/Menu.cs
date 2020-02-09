9b207e7323bab610c0b64162eb03da7f2a65ca79
Sai Charan Kommana <saicharan.k3899@gmail.com>
2020-02-09 10:23:33 +0530

    toggling UI in AR game with Vuforia

---
+++ UI in AR Project/Assets/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Walk()
+    {
+        animator.Play("walk", -1, 0f);
+    }
+
+    public void Jump()
+    {
+        animator.Play("jump", -1, 0f);
+    }
+
+    public void Hit1()
+    {
+        animator.Play("hit01", -1, 0f);
+    }
+
+    public void Hit2()
+    {
+        animator.Play("hit02", -1, 0f);
+    }
+}
